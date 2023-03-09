a76ccf5d2150e803f12ad1d02d9fdf0e46e3ba88
MiaMoore00 <miashiamoore@gmail.com>
2023-03-09 17:30:41 -0600

    base
    base

---
+++ plantapp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import InsectId from './InsectId';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+<InsectId /> 
+    </div>
+  );
+}
+
+export default App;
