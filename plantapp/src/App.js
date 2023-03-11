386ad7efeeba601bcd4e63ec7901831e2393a555
DaneenM <45468867+DaneenM@users.noreply.github.com>
2023-03-11 12:28:21 -0500

    Auth validation + Auth database

--- plantapp/src/App.js
+++ plantapp/src/App.js
@@ -5,6 +5,7 @@ import PlantId from "./PlantID";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path="register" element={<Register />} />
+        </Routes>
+        <Routes>
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </BrowserRouter>
     </div>
