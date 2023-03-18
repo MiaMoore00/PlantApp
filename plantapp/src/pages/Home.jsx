82bd434fbb08082013f2dd6c66879b0d7f3efc08
Lauren Whitted <lauren.whitted13@gmail.com>
2023-03-18 09:52:10 -0500

    added button/links to our home page for our main components

--- plantapp/src/pages/Home.jsx
+++ plantapp/src/pages/Home.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
 import './Home.css'
+import { Link } from "react-router-dom";
 
 
 function Home() {
   return (
     <div>Home
-      <SearchBar  />
-
-     
-    <h1 className="text-3xl font-bold underline text-green-700">
-      Super Duper Plant App  !
-    </h1>
-  
+      <SearchBar />
 
 
+      <h1 className="text-3xl font-bold underline text-green-700">
+        Super Duper Plant App  !
+      </h1>
+      <p><Link to="/PlantHealth"><button>
+        Assess your plant health
+      </button>
+      </Link>
+      </p>
+      <p><Link to="/PlantID"><button>
+        Get more info on your plant
+      </button>
+      </Link>
+      </p>
     </div>
   )
 }
